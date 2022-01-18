4f8b47f83aaf78876aaf43d43437c8da3c1959b3
leihub3 <isaia.luis@gmail.com>
2022-01-18 11:21:17 -0300

    I created the patients table. Not linked yet

---
+++ api/src/models/Patient.ts
@@ -0,0 +1,26 @@
+import {Model, Column, Table, CreatedAt, UpdatedAt} from 'sequelize-typescript';
+@Table
+export class Patient extends Model<Patient> {
+    @Column
+	firstName!: string;
+
+	@Column
+	lastName!: string;
+
+	@Column
+	email!: string;
+
+	@Column
+	phone!: number;
+
+	@Column
+	dni!: number;
+
+	@CreatedAt
+	@Column
+	createdAt!: Date;
+
+	@UpdatedAt
+	@Column
+	updatedAt!: Date;
+}
