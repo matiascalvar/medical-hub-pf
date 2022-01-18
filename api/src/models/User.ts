d9b95c1f7e39ffad2958918594f220cb89173e83
leihub3 <isaia.luis@gmail.com>
2022-01-18 16:46:08 -0300

    fix(api/src/models)
    Set active and isStaff columns with default values. Also remove "<User>"

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,15 +1,17 @@
-import {Model, Column, Table, CreatedAt, UpdatedAt} from 'sequelize-typescript';
+import {Model, Column, Table, CreatedAt, UpdatedAt, Default} from 'sequelize-typescript';
 @Table
-export class User extends Model<User> {
+export class User extends Model {//<User>
     @Column
 	email!: string;
 
 	@Column
 	hashedPass!: string;
 
+	@Default(true)
 	@Column
 	active!: boolean;
 
+	@Default(false)
 	@Column
 	isStaff!: boolean;
 
