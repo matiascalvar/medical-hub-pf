a3edd2a3dcee6929f247d59ae1d8f2f1678b279a
ColoCarletti <jcarletti@protonmail.com>
2022-01-18 21:22:01 -0300

    add accessToken

--- api/src/routes/login.ts
+++ api/src/routes/login.ts
@@ -1,24 +1,30 @@
-import {Response, Request, Router} from 'express';
+import { Response, Request, Router } from 'express';
 import  { User } from '../models/User'
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const router = Router();
+
+
 
 router.post('/', async (req, res) => {
     let response = await User.findOne({where: {email: req.body.email}})
     if (response) {
         try {
             if (await bcrypt.compare(req.body.password, response.hashedPass)) {
-                return res.send(response)
+                const user = { email: req.body.email }
+                const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+                
+                return res.send({ accessToken: accessToken })
             } else {
                 return res.status(401).send({"Error": "Contrasenia incorrecta."})
             }
-        } catch {
+        } catch(error) {
+            console.log(error)
             return res.sendStatus(500)
         }
     } else {
         return res.status(401).send({"Error": "No existe el usuario."})
     }
 });
-
 
 export default router;
