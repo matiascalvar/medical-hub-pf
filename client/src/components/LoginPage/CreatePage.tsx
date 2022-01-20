fb65aab63ad112cd1b11e8832bfc0112100a7aae
Jose Pardo <josmar.pm19@gmail.com>
2022-01-19 22:50:25 -0500

    (feat:component_login_register) add_components

--- client/src/components/LoginPage/CreatePage.tsx
+++ client/src/components/LoginPage/CreatePage.tsx
@@ -1,5 +1,53 @@
-const CreatePage = () => {
+import BackImg from '../../assets/LoginPage/back.jpg';
+import Logo from '../../assets/LoginPage/logo_sin.png';
+import "../../styles/LoginPage/CreatePage.css";
 
+const CreatePage = () => {
+  return(
+    <div className='containerCreate'>
+      <img src={BackImg} className='containerCreate__img'/>
+      <div className="containerCreate__register">
+        <img src={Logo} className='register__logo'/>
+        <div className="register__item">
+          <label className="item__title">EMAIL</label>
+          <div className="item__input">
+            <input type="text" className="input" />
+          </div>
+        </div>
+        <div className="register__item">
+          <label className="item__title">USERNAME</label>
+          <div className="item__input">
+            <input type="text" className="input" />
+          </div>
+        </div>
+        <div className="register__item">
+          <label className="item__title">LAST NAME</label>
+          <div className="item__input">
+            <input type="text" className="input" />
+          </div>
+        </div>
+        <div className="register__item">
+          <label className="item__title">NAME</label>
+          <div className="item__input">
+            <input type="text" className="input" />
+          </div>
+        </div>
+        <div className="register__item">
+          <label className="item__title">DNI</label>
+          <div className="item__input">
+            <input type="number" className="input" />
+          </div>
+        </div>
+        <div className="register__item">
+          <label className="item__title">CELLPHONE</label>
+          <div className="item__input">
+            <input type="number" className="input" />
+          </div>
+        </div>
+        <button className="register__button">REGISTER</button>
+      </div>
+    </div>
+  )
 }
 
 export default CreatePage;
--- client/src/components/LoginPage/LoginPage.tsx
+++ client/src/components/LoginPage/LoginPage.tsx
@@ -1,6 +1,48 @@
+import { Link } from 'react-router-dom';
+import {FaAt, FaLock} from 'react-icons/fa';
+import BackImg from '../../assets/LoginPage/back.jpg';
+import Logo from '../../assets/LoginPage/logo_sin.png';
+import "../../styles/LoginPage/LoginPage.css";
+
 const LoginPage = () => {
   return (
-    <h1>Holi</h1>
+    <div className='divContainer'>
+      <img src={BackImg} alt="fondo" className="imgFondo"/>
+      <div className="container">
+        <div className="container__logo">
+          <img src={Logo} className="logo__medicalHub" />
+        </div>
+        <div className="container__form">
+          <form className="form">
+            <div className='form__title'>
+              <img src={Logo} className="formLogo__item" />
+              <h2 className="formTitle__item">LOGIN</h2>
+            </div>
+            <div className="form__user">
+              <label className="user__title">EMAIL</label>
+              <div className="user__input">
+                <input type="text" className="input__item" />
+                <FaAt className='input__icons'/>
+              </div>
+            </div>
+            <div className="form__password">
+              <label className="password__title">PASSWORD</label>
+              <div className="password__input">
+                <input type="password" className="input__item"/>
+                <FaLock className='input__icons'/>
+              </div>
+            </div>
+            <div className="form__register">
+              <Link 
+                to="/register" 
+                className="btn__register"
+              >You are not registered yet?</Link>
+            </div>
+            <button className="form__btn">GET INTO</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
