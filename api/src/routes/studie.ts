a69d8e2faab48a293e5698316de27882092b6a23
NessDisk <ness.cero1@gmail.com>
2022-01-20 14:57:20 -0500

    Fix Add studie - POST and GET

--- api/src/routes/studie.ts
+++ api/src/routes/studie.ts
@@ -11,59 +11,69 @@ const {Op} = require(`sequelize`)
 const router = Router();
 
 
-
 router.post('/', async (req, res) => {
     // Check all data.
-    // Example info  body
-    //     state: "ACTIVE",
-    //     diagnosis: "normal",
-    //     studyPDF: "https/rute",
-    //     StudyTypeName:"escaner",
-    //     MedicalStaffName:"Gustavo" ,
-    //     AppointmentDate:"05/06/2021",
-    //     Patientdni:"Laura",
-         
-// Verificate fork-Ids
-    //StudyType
-    const TriggerStudyType = await StudyType.findOne({where: {name: {
-        [Op.iLike]: "%"+req.body.StudyTypeName+"%"
-       } }})
-    //MedicalStaff
-    const TriggerMedicalStaff = await MedicalStaff.findOne({where: {name:{
-        [Op.iLike]: "%"+ req.body.MedicalStaffName+"%"
-       }
-        }})    
-    //Appointment
-    const  TriggerAppointment = await Appointment.findOne({where: {date: req.body.AppointmentDate}})
-    //Patient
-    const TriggerPatient = await Patient.findOne({where: {dni: req.body.PatientDni}})
+    // Verificate info
+        //StudyType
+        const TriggerStudyType = await StudyType.findOne(  
+            {
+                where: {
+                    name: {
+                             [Op.iLike]: "%"+req.body.StudyTypeName+"%"
+                          } 
+                        }
+            }
+               )
+            //MedicalStaff
+            const TriggerMedicalStaff = await MedicalStaff.findOne(
+                {       where: {
+                            name:{
+                            [Op.iLike]: "%"+ req.body.MedicalStaffName+"%"
+                                 }
+                                }
+                }
+                                                                    )    
+                //Appointment
+                const  TriggerAppointment = await Appointment.findOne(
+                    {where:
+                         {
+                        date: req.body.AppointmentDate
+                         }
+                    }       
+                                                                    )
+                //Patient
+                const TriggerPatient = await Patient.findOne(
+                    {
+                        where:
+                         {
+                             dni: req.body.PatientDni
+                        }
+                    }
+                                                            )
+            
+                if (
+                        TriggerStudyType === null
+                        ||TriggerMedicalStaff === null
+                        ||TriggerAppointment === null
+                        ||TriggerPatient === null 
+                        )
+                        {
+                                return res.status(403).send({"Error:": "falta valor en la request"})
+                            }                   
+                    try 
+                    {
 
-    if (
-        TriggerStudyType === null
-        ||TriggerMedicalStaff === null
-        ||TriggerAppointment === null
-        ||TriggerPatient === null 
-        )
-        {
-            return res.status(403).send({"Error:": "falta valor en la request"})
-        }  
 
-
-
-
-    try 
-        {
-        let estudie = 
-        {
+let estudie = {
              state: req.body.state,
              diagnosis: req.body.diagnosis,
              studyPDF: req.body.studyPDF,
-             pacientId: TriggerStudyType.id ,
-             medicalStaffId:TriggerMedicalStaff.id,
-             studyTypeId:TriggerStudyType.id,
-             appointmentId:TriggerAppointment.id
-        }
-        
+             PacientId: TriggerStudyType.id ,
+             MedicalStaffId:TriggerMedicalStaff.id,
+             StudyTypeId:TriggerStudyType.id,
+             AppointmentId:TriggerAppointment.id
+             }
+
         const response = await Studie.create(estudie)
      
         return res.status(201).send(response)
@@ -73,32 +83,34 @@ router.post('/', async (req, res) => {
     };
 
     
-    res.send("Hola mundo")
+   
 });
 
-//Get all detail about one studie
-router.get('/:name', async (req, res) => {
+//Get all detail about one studie, 
+//use params
+router.get('/:id', async (req, res) => {
     //get the req for the body name patient
 
-    const  findID = await Patient.findOne({where: {name: req.params.name }})
-
+    const  findID = await Patient.findOne({where: {name: req.params.id }})
+    
     if(findID === null)    
         return res.status(403).send({"Error:": "Paciente no existe en la database."})
-  
+    
     try 
     {
+       
         
         //find de module 
         const AllStudie  = await  Studie.findAll(
             { where:
-                {
-                    id : findID.id
+                {                  
+                   PatientId : req.params.id  
                 }
             }           
-        )
+                                               )
 
-        // send data, not shall nothing in the req.
         return res.status(201).send(AllStudie)
+        // send data, not shall nothing in the req.
     }
     catch(e) {
         console.log(e)
@@ -109,16 +121,6 @@ router.get('/:name', async (req, res) => {
 
 //Change info about Studie
 router.put('/', async (req, res) => {
-    
-    // Check all data.
-    // Example info  body
-    //     state: "ACTIVE",
-    //     diagnosis: "normal",
-    //     studyPDF: "https/rute",
-    //     StudyTypeName:"escaner",
-    //     MedicalStaffName:"Gustavo" ,
-    //     AppointmentDate:"05/06/2021",
-    //     Patientdni:"Laura",
 
 
 })
