007f59bcd54a2c0598ab0f3779d3f0cf8a610030
ColoCarletti <jcarletti@protonmail.com>
2022-01-20 21:21:34 -0300

    adds functionality

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent } from "react";
+import { useHistory } from "react-router-dom"
+import axios from 'axios';
 import "../../styles/LoginPage/Register.css";
 import {
   FaAt,
@@ -11,6 +13,50 @@ import {
 } from "react-icons/fa";
 
 const CreatePage: FunctionComponent = () => {
+
+    const history = useHistory()
+
+    const registerUser = async function(user: any) {
+        try {
+            const response = await axios.post('http://localhost:3001/register', user)
+            return response
+        } catch(error) {
+            console.log(error)
+            return error
+        }
+    }
+
+    const emptyInput = {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        dni: '',
+        phone: '',
+    }
+
+    const [ input, setInput ] = React.useState(emptyInput)
+
+    const handleInputChange = function(e: any) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async function(e: any) {
+        e.preventDefault()
+        const newUser: any = await registerUser(input)
+        if (newUser.status === 201) {
+            // Agregar cartel de: Agregado con exito
+            history.push('/login')
+        } else {
+            console.log('No se pudo agregar:')
+            console.log(newUser)
+        }
+        setInput(emptyInput)
+    }
+
   return (
     <div className="containerCreate">
       <div className="containerCreate__register">
@@ -28,6 +74,9 @@ const CreatePage: FunctionComponent = () => {
             <input
               type="text"
               className="register__input"
+              name="email"
+              value={input.email}
+              onChange={handleInputChange}
               placeholder="Email"
             />
             <FaAt className="register__icons" />
@@ -36,18 +85,30 @@ const CreatePage: FunctionComponent = () => {
             <input
               type="password"
               className="register__input"
+              name="password"
+              value={input.password}
+              onChange={handleInputChange}
               placeholder="Password"
             />
             <FaLock className="register__icons" />
           </div>
           <div className="register__item">
-            <input type="text" className="register__input" placeholder="Name" />
+            <input 
+              type="text" 
+              className="register__input"
+              name="firstName"
+              value={input.firstName}
+              onChange={handleInputChange}
+              placeholder="Name" />
             <FaUserAlt className="register__icons" />
           </div>
           <div className="register__item">
             <input
               type="text"
               className="register__input"
+              name="lastName"
+              value={input.lastName}
+              onChange={handleInputChange}
               placeholder="Last Name"
             />
             <FaUserCircle className="register__icons" />
@@ -56,6 +117,9 @@ const CreatePage: FunctionComponent = () => {
             <input
               type="number"
               className="register__input"
+              name="dni"
+              value={input.dni}
+              onChange={handleInputChange}
               placeholder="DNI"
             />
             <FaRegIdCard className="register__icons" />
@@ -64,6 +128,9 @@ const CreatePage: FunctionComponent = () => {
             <input
               type="number"
               className="register__input"
+              name="phone"
+              value={input.phone}
+              onChange={handleInputChange}
               placeholder="Phone"
             />
             <FaPhoneSquareAlt className="register__icons" />
@@ -82,7 +149,7 @@ const CreatePage: FunctionComponent = () => {
               </option>
             </select>
           </div>
-          <button className="register__button">REGISTER</button>
+          <button onClick={handleSubmit} className="register__button">REGISTER</button>
         </div>
       </div>
     </div>
