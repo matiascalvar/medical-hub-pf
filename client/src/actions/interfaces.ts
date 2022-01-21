45d17a06ae54d63c7b99fc89bff286cd9e686410
ColoCarletti <jcarletti@protonmail.com>
2022-01-21 11:25:56 -0300

    remove interface S and modify User

--- client/src/actions/interfaces.ts
+++ client/src/actions/interfaces.ts
@@ -1,12 +1,4 @@
-// Si esta interface genera problemas, creo que migrando a redux 3.9 se soluciona
-export interface S {
-    type: number,
-    payload: any
-}
-
-// Esta interface depende de la respuesta de nuestro Axios. 
 export interface User {
-	id: number;
-	name: string;
-	lastName: string;
+	email: string;
+	token: string;
 }
