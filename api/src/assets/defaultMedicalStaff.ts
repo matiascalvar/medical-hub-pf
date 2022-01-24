aac8c3a6c3850aac3a655b6f77f075753afef31f
leihub3 <isaia.luis@gmail.com>
2022-01-24 16:43:13 -0300

    password

--- api/src/assets/defaultMedicalStaff.ts
+++ api/src/assets/defaultMedicalStaff.ts
@@ -2,11 +2,25 @@ const medicalList = require('./medicalStafList.json')
 import { User } from '../models/User'
 import { Patient } from '../models/Patient'
 import { MedicalStaff } from '../models/MedicalStaff'
+import populateDB from './populate'
+import jsonSpecialities from './specialitiesJSON';
+// import populatePlans from './populatePlans'
 
-const hashedPass = '$2b$10$TEqPlvX6MQVI7QcOYU6taOS5QiyRXQwWFVoUa4UaKjzGbruxBX2D.'
-// Resultado de hashear "12345678"
+const hashedPass = '$2b$10$tmnj1HpZkixb7B2.2J7pAeP8.w.Wg2hgC1uPtL9UM9M8hZUQ8ZLYm'
+// Resultado de hashear "123456789"
 const defaultMedicalStaff = async () => {
-
+    
+    const specialities = await populateDB(jsonSpecialities)
+    //const plans = await populatePlans()
+    
+    let specQuantity: number[] = [];
+    let i=1;
+    specialities?.map(s => {specQuantity.push(i++)})
+    
+    // let plansQuantity: number[] = [];
+    // let x=1;
+    // plans?.map(s => {plansQuantity.push(x++)})
+  
     const usersFromDB = await User.findAll();
     if (usersFromDB.length === 0) { 
         medicalList.map( async (e:any) =>
@@ -25,7 +39,7 @@ const defaultMedicalStaff = async () => {
                 phone: e.phone,
                 dni: e.dni,
                 UserId: user.id,
-                PlanId: null
+                //PlanId: plansQuantity ? plansQuantity[Math.floor(Math.random() * plansQuantity?.length)] : null
             }
             const patient = await Patient.create(newPatient)
 
@@ -33,12 +47,14 @@ const defaultMedicalStaff = async () => {
                 UserId: user.id,
                 firstName: e.firstName,
                 lastName: e.lastName,
-                idNumber: e.dni
+                idNumber: e.dni,
+                SpecialitieId: specQuantity ? specQuantity[Math.floor(Math.random() * specQuantity?.length)] : null
             }
             const medic = await MedicalStaff.create(newMedic)
 
         }
         )
+
     }
 }
 
