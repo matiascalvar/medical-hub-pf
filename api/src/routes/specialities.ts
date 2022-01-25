5cc621758666425132d6a7e88a5365c634bb8de8
leihub3 <isaia.luis@gmail.com>
2022-01-25 11:56:42 -0300

    add route to get all medics by speciality id

--- api/src/routes/specialities.ts
+++ api/src/routes/specialities.ts
@@ -1,4 +1,5 @@
 import {Response, Request, Router} from 'express';
+import { MedicalStaff } from '../models/MedicalStaff';
 import { Specialitie } from '../models/Specialitie';
 import { authenticateToken } from './middelwares'
 const router = Router();
@@ -14,6 +15,27 @@ router.get('/', authenticateToken , async (req, res) => {
         console.log(error)
         res.status(404)
     }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const idSpeciality = req.params.id;
+
+        const medicalStaff = await MedicalStaff.findAll({
+            where: {SpecialitieId: idSpeciality},
+            attributes: {include:  ['id', 'firstName', 'lastName','idNumber'], exclude: ['availability', 'avbTo', 'avbFrom', 'appointmentDuration', 'SpecialitieId', 'UserId', 'createdAt','updatedAt']},
+            order: [
+                ['lastName', 'ASC']
+            ]
+        });         
+
+        medicalStaff.length > 0 ? res.send(medicalStaff) : res.send({message: "No hay médicos con esa Especialidad."})
+        
+    } catch (e) {
+        console.log(e)
+        return res.status(401).send({Error: e})
+    }
+    
 });
 
 export default router;
