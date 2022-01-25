22b319f84d7728d918b4a4ac0bb37583ae85a2ea
ColoCarletti <jcarletti@protonmail.com>
2022-01-25 09:17:37 -0300

    adds cleenen up the refreshToken table

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 // QUITAR FORCE: TRUE SI SE QUIERE CONSERVAR LAS TABLAS DE LA DB
 import populateDB from './src/assets/populate';
 import jsonSpecialities from '../api/src/assets/specialitiesJSON';
-
 import {sequelize} from './src/db';
 import app from './src/app';
 import defaultMedicalStaff from './src/assets/defaultMedicalStaff';
+import clearDB from './src/assets/clearDB'
+const cron = require('node-cron')
+
 sequelize
 	.sync({force: true, logging: false})
 	.then(() => {
@@ -18,3 +20,7 @@ sequelize
 
 	})
 	.catch((err) => console.error(err));
+
+cron.schedule('0 0 * * *', () => {
+    clearDB()
+})
