b27ff16f8f7dee9ea3d099c4457c7d512483082b
Lucas Barallobre <lucasbarallobre@gmail.com>
2022-01-26 17:53:58 -0300

    Modified specialities as Luis

--- api/src/routes/specialities.ts
+++ api/src/routes/specialities.ts
@@ -1,19 +1,48 @@
-import {Response, Request, Router} from 'express';
-import { Specialitie } from '../models/Specialitie';
-import { authenticateToken } from './middelwares'
+import { Response, Request, Router } from "express";
+import { MedicalStaff } from "../models/MedicalStaff";
+import { Specialitie } from "../models/Specialitie";
+import { authenticateToken } from "./middelwares";
 const router = Router();
 
+router.get("/", async (req, res) => {
+  try {
+    let specialities = await Specialitie.findAll();
+    res.status(200).send(specialities);
+  } catch (error) {
+    console.log(error);
+    res.status(404);
+  }
+});
 
-router.get('/', authenticateToken , async (req, res) => {
-    
-    try {
-        let specialities = await Specialitie.findAll()
-        res.status(200).send(specialities)
-    }
-    catch (error) {
-        console.log(error)
-        res.status(404)
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const idSpeciality = req.params.id;
+
+    const medicalStaff = await MedicalStaff.findAll({
+      where: { SpecialitieId: idSpeciality },
+      attributes: {
+        include: ["id", "firstName", "lastName", "idNumber"],
+        exclude: [
+          "availability",
+          "avbTo",
+          "avbFrom",
+          "appointmentDuration",
+          "SpecialitieId",
+          "UserId",
+          "createdAt",
+          "updatedAt",
+        ],
+      },
+      order: [["lastName", "ASC"]],
+    });
+
+    medicalStaff.length > 0
+      ? res.send(medicalStaff)
+      : res.send({ message: "No hay médicos con esa Especialidad." });
+  } catch (e) {
+    console.log(e);
+    return res.status(401).send({ Error: e });
+  }
 });
 
 export default router;
