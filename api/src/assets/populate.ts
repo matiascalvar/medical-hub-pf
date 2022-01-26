65172f19f53b63f56b0d30b24664ac22aa626cec
Lucas Barallobre <lucasbarallobre@gmail.com>
2022-01-26 17:52:38 -0300

    Modified populate because older doens't works

--- api/src/assets/populate.ts
+++ api/src/assets/populate.ts
@@ -1,14 +1,11 @@
-import { Specialitie } from "../models/Specialitie"
+import { Specialitie } from "../models/Specialitie";
 
 export default async function populateDB(params: []) {
-    
-    const dataFromDB = await Specialitie.findAll();
-    if (dataFromDB.length === 0) {
-        
-        let arrParsed: Array<object> = []
-        params.map(e => arrParsed.push(JSON.parse(e)))
-        Specialitie.bulkCreate(arrParsed)
-        return arrParsed
-    }
-   
+  const dataFromDB = await Specialitie.findAll();
+  if (dataFromDB.length === 0) {
+    let arrParsed: Array<any> = [];
+    params.map((e) => arrParsed.push(JSON.parse(e)));
+    Specialitie.bulkCreate(arrParsed);
+    return arrParsed;
+  }
 }
