9dfcc4bb71a20db28b7dd555939e538d33937d19
matiasncalvar <matiasncalvar@gmail.com>
2022-01-27 08:11:56 -0300

    sequelize path to online db

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,66 +1,69 @@
-import {Sequelize} from 'sequelize-typescript';
-import config from '../lib/config';
-config;
-export const sequelize = new Sequelize({
-	dialect: 'postgres',
-	database: config.dbName,
-	password: config.dbPassword,
-	username: config.dbUser,
-	storage: ':memory:',
-	models: [__dirname + '/models'],
-	logging: false
-});
+import { Sequelize } from "sequelize-typescript";
 
+export const sequelize = new Sequelize(
+  "postgres://yytgfamu:o7PCNaSN1dGtgQVIPHEUYsMmmcdloHQm@kesavan.db.elephantsql.com/yytgfamu",
+  {
+    models: [__dirname + "/models"],
+    logging: false,
+  }
+);
 
-const { User, Patient, Plan, Appointment, AppointmentDetail, MedicalStaff, Specialitie, Studie, StudyType} = sequelize.models;
+const {
+  User,
+  Patient,
+  Plan,
+  Appointment,
+  AppointmentDetail,
+  MedicalStaff,
+  Specialitie,
+  Studie,
+  StudyType,
+} = sequelize.models;
 
+Patient.belongsTo(User, { targetKey: "id" });
+User.hasOne(Patient, { sourceKey: "id" });
 
-Patient.belongsTo(User, { targetKey: "id" } )
-User.hasOne(Patient, { sourceKey: "id" })
-
-Plan.hasOne(Patient, { sourceKey: "id" })
-Patient.belongsTo(Plan, { targetKey: "id" })
+Plan.hasOne(Patient, { sourceKey: "id" });
+Patient.belongsTo(Plan, { targetKey: "id" });
 
 Patient.hasMany(Appointment, {
-	sourceKey: "id",
-  	foreignKey: "PatientId"
-})
+  sourceKey: "id",
+  foreignKey: "PatientId",
+});
 
 Specialitie.hasMany(MedicalStaff, {
-	sourceKey: "id",
-  	foreignKey: "SpecialitieId"
-})
-MedicalStaff.belongsTo(Specialitie)
+  sourceKey: "id",
+  foreignKey: "SpecialitieId",
+});
+MedicalStaff.belongsTo(Specialitie);
 
 MedicalStaff.belongsTo(User, { targetKey: "id" });
 User.hasOne(MedicalStaff, { sourceKey: "id" });
 
 MedicalStaff.hasMany(Appointment, {
-	sourceKey: "id",
-  	foreignKey: "MedicalStaffId"
-})
-Appointment.belongsTo(MedicalStaff)
+  sourceKey: "id",
+  foreignKey: "MedicalStaffId",
+});
+Appointment.belongsTo(MedicalStaff);
 
-// Studies retaltions 
+// Studies retaltions
 StudyType.hasOne(Studie, { sourceKey: "id" });
-Studie.belongsTo(StudyType, { targetKey: "id" })
+Studie.belongsTo(StudyType, { targetKey: "id" });
 
 MedicalStaff.hasMany(Studie, {
-	sourceKey: "id",
-  	foreignKey: "MedicalStaffId"
-})
+  sourceKey: "id",
+  foreignKey: "MedicalStaffId",
+});
 Studie.belongsTo(MedicalStaff);
 
 Appointment.hasMany(Studie, {
-	sourceKey: "id",
-  	foreignKey: "AppointmentId"
+  sourceKey: "id",
+  foreignKey: "AppointmentId",
 });
 Studie.belongsTo(Appointment);
 
 Patient.hasOne(Studie, { sourceKey: "id" });
 Studie.belongsTo(Patient, { targetKey: "id" });
 
-Appointment.hasOne(AppointmentDetail, { sourceKey: "id" })
-AppointmentDetail.belongsTo(Appointment, { targetKey: "id" })
-
-
+Appointment.hasOne(AppointmentDetail, { sourceKey: "id" });
+AppointmentDetail.belongsTo(Appointment, { targetKey: "id" });
