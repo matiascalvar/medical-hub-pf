caa475db935ff67ac99cca9b8b713cf72d3b089e
leihub3 <isaia.luis@gmail.com>
2022-01-27 19:07:30 -0300

    new route to get the availables appo by speciality

--- api/src/routes/appointments.ts
+++ api/src/routes/appointments.ts
@@ -62,14 +62,23 @@ function addDays(date: any, days: number) {
     dates.setDate(dates.getDate() + days);
     const array = dates.toLocaleDateString().toString().split('/');
     let day: string ='';
-    array[1].length < 2 ? day = '0' + array[1] : day = array[1]
-    const formatedDate = array[2] + '-' + (array[0].length<2 && '0'+array[0]) + '-' + day;
+    let month: string ='';
+    array[1].length < 2 ? day = '0' + array[1] : day = array[1];
+    array[0].length < 2 ? month = '0' + array[0] : month = array[0];
+
+    const formatedDate = array[2] + '-' + month + '-' + day;
     return formatedDate.toString();
 }
 
 router.get('/avb/:idMedicalStaff', async (req, res) => {        
     try {
         const { idMedicalStaff } = req.params;
+
+        const medic = await MedicalStaff.findOne({
+            where: {
+                id: idMedicalStaff
+            }
+        })
 
         const date = new Date();
         const totalDays: number = 1;
@@ -116,11 +125,13 @@ router.get('/avb/:idMedicalStaff', async (req, res) => {
                         availability.avb.splice(index, 1);
                         }     
             })
-
             result.push(availability)
         }
 
-        res.send(result)
+        res.send({
+            medic: medic?.firstName + " " + medic?.lastName,
+            data: result
+        })
            
     } catch (e) {
         console.log(e)
@@ -128,5 +139,90 @@ router.get('/avb/:idMedicalStaff', async (req, res) => {
     }
     
 });
+
+
+router.get('/avbspeciality/:idSpeciality', async (req: Request, res: Response) => {
+    try {
+        const { idSpeciality } = req.params;
+
+        const date = new Date();
+        const totalDays: number = 1; //modificar segun la cantidad de dias a mostrar
+
+        let result: any[] = [];
+        let resultToShow: any[] = [];
+
+        const medicsOfThisSpeciality = await MedicalStaff.findAll({ //consulto los medicos de la esoecialidad
+            where:{
+                SpecialitieId: idSpeciality
+            },
+            attributes: {include:  ['id', 'firstName', 'lastName','idNumber'], exclude: ['availability', 'avbTo', 'avbFrom', 'appointmentDuration', 'SpecialitieId', 'UserId', 'createdAt','updatedAt']}
+        })
+
+       medicsOfThisSpeciality.map(m => result.push(m)) //lleno la cosntante result con los medicos de la especialidad
+
+        for(let i=0; i< totalDays; i++){            
+
+            const today = addDays(date,i); // today se va a ir modificando segun la cant de dias que contenga totalDays
+
+            const allAppointments = await Appointment.findAll({//consultamo todos los appointments     
+                where:{
+                    date: today
+                },           
+                include: [
+                        {
+                            model: MedicalStaff
+                        }
+                    ]
+            }); 
+
+            let availability: any = { //armamos el calendario inicializando cada horario con los medicos de la especialidad
+                fecha: today,
+                avb: {
+                    '09:00:00': [...result],
+                    '09:30:00': [...result],
+                    '10:00:00': [...result],
+                    '10:30:00': [...result],
+                    '11:00:00': [...result],
+                    '11:30:00': [...result],
+                    '12:00:00': [...result],
+                    '12:30:00': [...result],
+                    '13:00:00': [...result],
+                    '13:30:00': [...result],
+                    '14:00:00': [...result],
+                    '14:30:00': [...result],
+                    '15:00:00': [...result],
+                    '15:30:00': [...result],
+                    '16:00:00': [...result],
+                    '16:30:00': [...result],
+                    '17:00:00': [...result],
+                    '17:30:00': [...result]
+                }               
+            }
+    
+            allAppointments.map((a,i) => {//mapeamos los appointmnets
+                let objProp: string = a.time.toString();
+                    if(availability.avb[objProp]){ // si encontramos un turno para este horaio...
+                        if(a?.toJSON().MedicalStaff.SpecialitieId == idSpeciality && a?.toJSON().date == today){//...para esta especialidad y dia ...
+                                result.map(m => {//..mapeamos los medicos
+                                    if(m.id == a?.toJSON().MedicalStaff.id) {//quitamos del calendario al medico que NO esta disponible
+                                        var index = availability.avb[objProp].indexOf(m);
+                                            if (index !== -1) {
+                                            availability.avb[objProp].splice(index, 1);
+                                            }
+                                    }
+                                })
+                        }                        
+                    }              
+            })
+            resultToShow.push(availability)//en la constante resultToSHow vamos guardando el calendario dia por dia
+        }    
+        
+        res.send(resultToShow)
+
+    } catch (error) {
+        res.send(error)
+    }
+
+})
 
 export default router;
