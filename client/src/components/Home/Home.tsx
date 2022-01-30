9ddd60d48099de4d68d7b22ba62f5b5bce77f8c2
ColoCarletti <jcarletti@protonmail.com>
2022-01-30 18:43:11 -0300

    adds functionality to modify appointment payment

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import UserHome from "./UserHome/UserHome";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getUserInfo } from "../../actions/index";
+import axios from "axios";
 
 interface Info {
   firstName: any;
@@ -11,25 +12,49 @@ interface Info {
 }
 
 export default function Home(): JSX.Element {
-  const activeUser = useSelector((state: any) => state.user);
-  const response = useSelector((state: any) => state.userInfo);
-  const dispatch = useDispatch();
-  const [myInfo, setMyInfo] = useState<Info>({
-    firstName: "",
-    lastName: "",
-  });
-  console.log(activeUser);
-  useEffect(() => {
-    if (response) {
-      setMyInfo({
-        firstName: response.firstName,
-        lastName: response.lastName,
-      });
+    const activeUser = useSelector((state: any) => state.user);
+    const response = useSelector((state: any) => state.userInfo);
+    const dispatch = useDispatch();
+    const [myInfo, setMyInfo] = useState<Info>({
+        firstName: "",
+        lastName: "",
+    });
+    const updateAppointment = async (activeUser: any, id: number, data: any) => {
+        const headers = {
+          headers: {
+            Authorization: activeUser.token,
+            Accept: "aplication/json",
+          },
+        };
+        const authAxios = axios.create(headers);
+        try {
+          const response = await authAxios.put(`http://localhost:3001/appointments/${id}`, data)
+        } catch (error) {
+            console.log(error)
+        }
     }
-    if (activeUser.email && !myInfo.firstName) {
-      dispatch(getUserInfo(activeUser));
-    }
-  }, [response, activeUser]);
+      
+    
+    useEffect(() => {
+        let url = new URL(window.location.href)
+        let params = new URLSearchParams(url.search)
+        let status = params.get("status")
+        if (status === "approved") {
+            let appointmentId = parseInt(params.get("external_reference")!)    
+            updateAppointment(activeUser, appointmentId, {pay: true})
+        }
+        console.log(status)
+        if (response) {
+            setMyInfo({
+                firstName: response.firstName,
+                lastName: response.lastName,
+            });
+        }
+        if (activeUser.email && !myInfo.firstName) {
+            dispatch(getUserInfo(activeUser));
+        }
+
+    }, [response, activeUser]);
 
   if (myInfo.firstName) {
     return (
