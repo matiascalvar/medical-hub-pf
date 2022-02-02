24caf74de77071f00e61e7d6237c3c447e7f21dd
matiasncalvar <matiasncalvar@gmail.com>
2022-02-02 08:41:59 -0300

    webhook added to mercadopago

--- api/src/routes/mercadopago.ts
+++ api/src/routes/mercadopago.ts
@@ -1,5 +1,7 @@
 import { Response, Request, Router } from "express";
+import { Appointment } from "../models/Appointment";
 const mercadopago = require("mercadopago");
+const axios = require("axios");
 const router = Router();
 
 // Token creado desde las credenciales de Mercado Pago
@@ -14,8 +16,6 @@ mercadopago.configure({
 router.get("/", async (req: any, res: Response) => {
   let { appointmentId, unit_price, title } = req.query;
   unit_price = Number(unit_price);
-
-  // console.log("QUERY: ", unit_price, quantity, title); // Test
 
   mercadopago.preferences
     .create({
@@ -35,11 +35,45 @@ router.get("/", async (req: any, res: Response) => {
       ],
     })
     .then((preference: any) => {
-      console.log(!"PREFERENCE: ", preference); // Test
-      // return res.json({ preferenceId: preference.body.id });
       return res.json({ preferenceId: preference.response.sandbox_init_point });
     })
     .catch((e: Error) => console.log(e));
+});
+
+router.post("/", async (req: any, res: Response) => {
+  console.log("id: ", req.body.data.id);
+  let paymentId = req.body.data.id;
+
+  try {
+    const headers = {
+      headers: {
+        Authorization:
+          "Bearer TEST-3165795748532207-012420-5ce03fdac737c3769cd0c770d40a9972-131457142",
+      },
+    };
+    const authAxios = axios.create(headers);
+    const response = await authAxios.get(
+      `https://api.mercadopago.com/v1/payments/${paymentId}`
+    );
+    console.log("STATUS DE PAGO", response.data.status);
+    let paymentStatus = response.data.status;
+    let paidAppointmentId = response.data.external_reference;
+    if (paymentStatus === "approved") {
+      try {
+        let appointment: any = await Appointment.findOne({
+          where: { id: paidAppointmentId },
+        });
+        const result = await appointment.update({ pay: true });
+        return res.status(201).send({ message: "Pago acreditado con exito" });
+      } catch (err) {
+        console.log(err);
+        return res.sendStatus(404);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(404);
+  }
 });
 
 export default router;
