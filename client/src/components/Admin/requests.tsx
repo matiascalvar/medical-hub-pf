ab487c65373fb1b221548c758204128f6fe61704
ColoCarletti <jcarletti@protonmail.com>
2022-02-03 11:26:17 -0300

    adds request all medics script

--- client/src/components/Admin/requests.tsx
+++ client/src/components/Admin/requests.tsx
@@ -1,11 +1,24 @@
 import axios from "axios";
 
+const URL = "http://localhost:3001"
+
+
 export const addMedic = async (input: any) => {
     try {
-        const response = await axios.post("http://localhost:3001/medicalstaff", input);
-        return response;
+        const response = await axios.post(URL + "/medicalstaff", input)
+        return response
     } catch (error) {
-        console.log(error);
-        return error;
+        console.log(error)
+        return error
+    }
+}
+
+export const getMedics = async () => {
+    try {
+        const response = await axios.get(URL + "/medicalstaff")
+        return response
+    } catch (error) {
+        console.log(error)
+        return error
     }
 }
