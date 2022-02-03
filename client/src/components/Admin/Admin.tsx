4464965b079e4839281f6682ed948003b868540d
ColoCarletti <jcarletti@protonmail.com>
2022-02-03 11:26:51 -0300

    adds MedicsTable

--- client/src/components/Admin/Admin.tsx
+++ client/src/components/Admin/Admin.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { displayPartsToString } from "typescript";
 import { } from "../../actions/index";
 import AddMedic from "./AddMedic";
+import MedicsTable from "./MedicsTable";
+import { getMedics } from "./requests"
 
 
 export default function Admin() : JSX.Element {
 
+    const [ display, setDisplay ] : any = React.useState({
+        component: "",
+        data: []
+    })
+
+    async function showMedicsTable() {
+        const reponse: any = await getMedics()
+        setDisplay({
+            component: "MedicsTable",
+            data: reponse.data
+        })
+    }
 
     return(
         <div>
             <h2>Admin page:</h2>
-            <AddMedic />
+            <button onClick={()=> setDisplay({component: "AddMedic", data: []})}>Add Medic</button>
+            <button onClick={()=> showMedicsTable()}>Medic list</button>
+            <>{display.component === "AddMedic"? <AddMedic/> : null}</>
+            <>{display.component === "MedicsTable"? <MedicsTable medics={display.data}/> : null}</>
         </div>
     )
 }
