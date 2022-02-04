f96d7efd760dbe00e10753ad13115d90a9e4838c
ColoCarletti <jcarletti@protonmail.com>
2022-02-04 16:54:30 -0300

    add list and details for appointments

--- client/src/components/Admin/Appointments/Appointments.tsx
+++ client/src/components/Admin/Appointments/Appointments.tsx
@@ -1,53 +1,86 @@
 import { useState, useEffect } from "react";
-import { getPatients } from "../requests";
+import { getAppointments } from "../requests";
 import AppointmentsDetail from "./AppointmentsDetail";
 
 export default function Appointments() : JSX.Element {
 
-    class Patient {
+    class Appointment {
         id: number;
-        name: string;
-        phone: number;
-        dni: number;
-
-        constructor (id: number, name: string, phone: number, dni: number) {
+        patient: string;
+        medic: string;
+        date: number;
+        time: number;
+        state: string;
+        pay: boolean;
+        
+        constructor (id: number, patient: string, medic: string, date: number, time: number, state: string, pay: boolean) {
             this.id = id;
-            this.name = name;
-            this.phone = phone;
-            this.dni = dni;
+            this.patient = patient;
+            this.medic = medic;
+            this.date = date;
+            this.time = time;
+            this.state = state;
+            this.pay = pay;
         };
     }
-    const [ patientList, setPatientList ] = useState<any> ();
+    const [ appointmentList, setAppointmentList ] = useState<any> ();
     const [ toDisplay, setToDisplay ] = useState<any> ();
     const [ searchInput, setSearchInput ] = useState("");
+    const [ filterDate, serFilterDate ] = useState("");
+    const [ filterState, setFilterState ] =useState("");
     const [ detailed, setDetailed] = useState<number> ();
 
     const createList = async () => {
-        const response: any = await getPatients()
+        console.log("entra a CreateList")
+        const response: any = await getAppointments()
         if (response) {
-            const patientList: any[] = []
-            response.data.forEach((patient: any) => patientList.push(new Patient(
-                patient.id,
-                `${patient.lastName}, ${patient.firstName}`,
-                patient.phone,
-                patient.dni
+            const appointmentList: any[] = []
+            response.data.forEach((appointment: any) => appointmentList.push(new Appointment(
+                appointment.id,
+                `${appointment.Patient.firstName}, ${appointment.Patient.lastName}`,
+                `${appointment.MedicalStaff.firstName}, ${appointment.MedicalStaff.lastName}`,
+                appointment.date,
+                appointment.time,
+                appointment.state,
+                appointment.pay
             )))
-            setPatientList(patientList)
-            setToDisplay(patientList)
+            setAppointmentList(appointmentList)
+            setToDisplay(appointmentList)
         }
     }
 
     const handleSearch = (e: any) => {
         setSearchInput(e.target.value)
-        if (parseInt(e.target.value)) {
-            updateDisplay(e.target.value, "dni")
-        } else {
-            updateDisplay(e.target.value, "name")
-        }
+        updateDisplay(e.target.value, "search")
+    }
+
+    const filterByState = (e: any) => {
+        setFilterState(e.target.value)
+        updateDisplay(e.target.value, "state")
+    }
+
+    const filterByDate = (e: any) => {
+        serFilterDate(e.target.value)
+        updateDisplay(e.target.value, "date")
     }
   
-    const updateDisplay = (input: string, parameter: string) => {
-        setToDisplay(patientList.filter((patient: any) => patient[parameter].includes(input)))
+    const updateDisplay = (input: string, toUpdate: string) => {
+        const filters = {
+            search: searchInput,
+            state: filterState,
+            date: filterDate,
+        }
+        if (toUpdate === "search") filters.search = input;
+        if (toUpdate === "state") filters.state = input;
+        if (toUpdate === "date") filters.date = input;
+
+        let display = appointmentList
+
+        if (filters.search) display = display.filter((a: any) => (a.medic.includes(filters.search) || a.patient.includes(filters.search)))
+        if (filters.state && filters.state != "ALL") display = display.filter((a: any) => a.state === filters.state)
+        if (filters.date) display = display.filter((a: any) => a.date === filters.date)
+
+        setToDisplay(display)
     }
 
     const reload = () => {
@@ -61,14 +94,22 @@ export default function Appointments() : JSX.Element {
 
     return (
         <div>
-            <h2>Patients:</h2>
+            <h2>Appointments:</h2>
             {detailed? <AppointmentsDetail id={detailed} return={() => setDetailed(undefined)} reolad={() => reload()}/> : 
                 <div>
                     <div>
+                        <label htmlFor="state">State: </label>
+                        <select onChange={filterByState} name="state">
+                            <option>ALL</option> 
+                            <option>ACTIVE</option>  
+                            <option>COMPLETED</option>  
+                        </select>
+                        <label htmlFor="date">Date:</label>
+                        <input onChange={filterByDate} name="date" type="date" />
                         <input 
                             onChange={handleSearch}
                             type="text"
-                            placeholder="Search by name or dni..."
+                            placeholder="Search by patient or medic..."
                             name="search"
                             value={searchInput}
                         />
@@ -78,17 +119,24 @@ export default function Appointments() : JSX.Element {
                         <tbody>
                             <tr>
                                 <th>ID</th>
-                                <th>Name</th>
-                                <th>Phone</th>
-                                <th>DNI</th>
+                                <th>Patient</th>
+                                <th>Medic</th>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>State</th>
+                                <th>Pay</th>
                             </tr>
-                            {toDisplay.map((patient: any)=>
+                            {toDisplay.map((appointment: any)=>
                                 <tr>
-                                    <td>{patient.id}</td>
-                                    <td>{patient.name}</td>
-                                    <td>{patient.phone}</td>
-                                    <td>{patient.dni}</td>
-                                    <td onClick={() => setDetailed(patient.id) }>Details</td>
+                                    <td>{appointment.id}</td>
+                                    <td>{appointment.patient}</td>
+                                    <td>{appointment.medic}</td>
+                                    <td>{appointment.date}</td>
+                                    <td>{appointment.time}</td>
+                                    <td>{appointment.state}</td>
+                                    <td>{appointment.pay}</td>
+
+                                    <td onClick={() => setDetailed(appointment.id) }>Details</td>
                                 </tr>
                             )}
                         </tbody>
