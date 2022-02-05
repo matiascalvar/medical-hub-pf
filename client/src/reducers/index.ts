e53ecc0c8ea4bde85b97bbb4a48ef3bba8239996
matiasncalvar <matiasncalvar@gmail.com>
2022-02-05 00:43:33 -0300

    bug with logout fixed

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -28,9 +28,7 @@ export default function reducer(state = initialState, action: actionI) {
         user: action.payload,
       };
     case ActionTypes.logout:
-      return {
-        initialState,
-      };
+      return {...initialState}
     case ActionTypes.getPatientInfo:
       return {
         ...state,
